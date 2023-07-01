36975f3df9947d0ab2511ac40c312ea1a81adcbe
Kenichi Matsuo <kenichimatsuo@kenichis-mbp.lan>
2023-06-30 23:28:50 -0700

    readme update

--- reinforcement_learning/dqn_snake/Deep_Q_README.md
+++ reinforcement_learning/dqn_snake/Deep_Q_README.md
@@ -1,12 +1,10 @@
-## Walk through
-This is currently a work in progress. This one week project is taking a bit longer hahahaha
-I'm spending most of my time trying to understand rl which is taking up a chunk of time
-currently I've implemented experience reply and targeted models; so I believe I'm almost done.
-I noticed i made a mistake with how i designed my model which is causing the 
-model to not converge.
+# Howdie
+Howdie! chase_food is from following a sent dex tutorial where I learned about q learning w/o incorporating deep learning
+Now I've moved onto learning about DQN. I've invested a bit of time reading relevant parts of Sutton's textbook
+The idea is to use convolutional NN with q learning to train a snake. Before finishing that part,
+I've also uploaded my code where I've trained snake using a regular linear NN where the state is represented by a head-relative array
 
-
-## How we represent the state
+## How We Represent The State
 - We can either have the N x N grid itself be a state where we assign values
 to the cell if we have a snake body it's a 0 snake head would be 1, food is a 2 and
 3 for an empty cell. This would be really easy and we could use CNN 
@@ -15,13 +13,32 @@ to the cell if we have a snake body it's a 0 snake head would be 1, food is a 2 
 the state of the game in a 1D array which contains info about the head's relative
 position to the food and any obstacles
 
+## General Algorithm:
+- I have a regular model and a trailing model. The regular model updates with each iteration but the trailing model
+copies the regular model after k-iterations the reason is that we don't want to "chase our own tail," thus I've implemented a replay buffer
+for each episode:
+    for each iteration in an episode:
+        save old state
+        pick an action
+        now save new state
+        obtain the reward after taking action a
+        if replay_buffer size > 128:
+            sample from replay buffer
+            calculate q values based on trailing nn and update the regular NN
+            
+            if its k iteration copy params from model into trailing model
+
 
 ## Interesting Errors I've hit
-I originally had an issue where the snake's rewards over time on average remained flat even after i trained it for approximately 5 million iterations
-What I next experimented with was using "experience replay" 
+I originally had an issue where the snake's rewards over time on average remained flat even after i trained it for approximately 5
+million iterations for the CNN
+
+I believe it's because the NN does not know which direction we're going when we submit the n x n grid as the input
+The Volodymyr Mnih 2013 Atari paper mentioned that they fed k=3-4 states into the NN which indirectly encodes direction.
+I've also been constantly been using 1 input channel and the Atari paper uses 4 input channels. Perhaps using greyscale isn't sufficient
+
+
 
 
 https://towardsdatascience.com/deep-q-network-dqn-ii-b6bf911b6b2c
-
-
 https://www.youtube.com/watch?v=KuXjwB4LzSA
--- reinforcement_learning/dqn_snake/Main.py
+++ reinforcement_learning/dqn_snake/Main.py
@@ -11,176 +11,27 @@ import matplotlib.pyplot as plt
 import Functions as f
 import os
 
-epsilon_decay_rate = 0.00005
+tick_rate = 10
+epsilon_decay_rate = 0.9999999
 epsilon_min = 0.02
-epsilon = 1.0
+epsilon = 0.4
+original_epsilon = epsilon
+temp_decay = 2.5*original_epsilon/game.num_episodes
+input_size = 16
+game_size = 20 #bigger because less dimensionality compared to cnn 
+def decay_epsilon(epsilon, epsilon_min, epsilon_decay_rate):
+    epsilon = max(epsilon_min, epsilon * epsilon_decay_rate)
+    return max(epsilon, epsilon_min)
 
-discount_rate = 0.85
-model = dqn.CNN_DQN(game.game_size)
-model_path = "model_parameters_no_discount.pth"
+discount_rate = 0.99
+model = dqn.CNN_DQN_V3(input_size)
+target_model = dqn.CNN_DQN_V3(input_size)
+model_path = "CNN_model_params.pth"
+
 if os.path.exists(model_path):
     model.load_state_dict(torch.load(model_path))
+    target_model.load_state_dict(torch.load(model_path))
     print("Loaded "+model_path)
-
-loss_fn = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=0.0001)
-
-
-display_rate = game.display_rate
-
-#function 
-def get_cnn_state(snake_body, food):
-    grid = [[0] * game.game_size for _ in range(game.game_size)]
-    for body in snake_body:
-        if body.is_out_of_bounds() == False:
-            grid[body.x][body.y] = 1
-    grid[food.x][food.y] = 2
-    return grid
-#function
-
-def choose_action(state):
-    state_tensor = torch.tensor(state, dtype=torch.float32).unsqueeze(0)
-    if np.random.rand() <= epsilon:
-        return np.random.randint(1,4)
-    else:
-        #print("CNN RESULT")
-        with torch.no_grad():
-            q_values = model(state_tensor)
-            #print(q_values)
-            #actions = torch.argmax(q_values, dim=1)
-            #print(actions)
-            #action = actions[0].item()
-            action = torch.argmax(q_values)
-            return action
-
-def take_action(action, snake_body, food):
-    #print("ACTION TAKEN: "+str(action))
-    if snake_body[-1].get_coord() == food.get_coord():
-        new_location = snake_body[-1].theoretical_action(action)
-        tail = snake_body[0]
-        new_head = Entity.Entity(game.game_size, new_location[0], new_location[1])
-        snake_body.append(new_head)
-        return True
-    else:
-        new_location = snake_body[-1].theoretical_action(action)
-        tail = snake_body.pop(0)
-        tail.x, tail.y = new_location
-        snake_body.append(tail)
-        return False
-
-def get_reward(snake_body, food, old_location):
-    def is_dead():
-        head = snake_body[-1]
-        if head.is_out_of_bounds():
-            return True
-        for body in snake_body[:-1]:
-            if body.get_coord() == head.get_coord():
-                return True
-        return False
-    if is_dead():
-        return game.death_penalty
-    head = snake_body[-1]
-    if head.get_coord() == food.get_coord():
-        return game.eat_reward
-    old_sq_distance = (old_location[0] - food.x)**2 + (old_location[1] - food.y)**2
-    new_sq_distance = (head.x - food.x)**2 + (head.y - food.y)**2
-    if old_sq_distance > new_sq_distance:
-        return game.closer_reward
-    return game.further_penalty
-
-
-################################################
-try:
-    reward_list = []
-    for episode in range(game.num_episodes):
-        food = Entity.Entity(game.game_size, np.random.randint(0,game.game_size),np.random.randint(0,game.game_size))
-        head = Entity.Entity(game.game_size, np.random.randint(0,game.game_size),np.random.randint(0,game.game_size))
-        snake_body = [head]
-        speed = (0,1)
-        episode_reward_count = 0
-
-        if episode % display_rate == 0 :
-            clock = pygame.time.Clock()
-            pygame.init()
-            display = pygame.display.set_mode((game.game_size * game.pixel_size, game.game_size * game.pixel_size))
-            display.fill(game.black)
-
-        for iter in range(game.iters_per_ep):    
-            old_state = get_cnn_state(snake_body, food)
-            old_head_location = snake_body[-1].get_coord()
-            action = choose_action(old_state)
-            
-            ate_apple = take_action(action, snake_body, food)
-            if ate_apple:
-                food = Entity.Entity(game.game_size, np.random.randint(0,game.game_size),np.random.randint(0,game.game_size))
-                get_new_loc = True
-                while get_new_loc:
-                    continue_loop = False
-                    for body in snake_body:
-                        if body.get_coord() == food.get_coord():
-                            continue_loop = True
-                            food = Entity.Entity(game.game_size, np.random.randint(0,game.game_size),np.random.randint(0,game.game_size))
-                            break
-                    get_new_loc = continue_loop
-                #check for bad replacement
-
-            #MUST CHANGE FOOD LOCATION and increment the size of the blob
-
-            iter_reward = get_reward(snake_body, food, old_head_location)
-            new_state = get_cnn_state(snake_body, food)
-            episode_reward_count += iter_reward
-
-
-
-            optimizer.zero_grad()
-            old_state_tensor = torch.tensor(old_state, dtype=torch.float32).unsqueeze(0) #add extra dimension
-            new_state_tensor = torch.tensor(new_state, dtype=torch.float32).unsqueeze(0)
-            
-            if (episode%500 == 0):
-                print("EPISODE: "+str(episode)+" ITERATION: "+str(iter))
-                print(model(old_state_tensor))
-                print("EPSILON: "+str(epsilon))
-                print("rewards: "+str(reward_list[-10:]))
-            #old_q_value = model(old_state_tensor)[0][action]
-            old_q_value = model(old_state_tensor)[action].unsqueeze(0)
-            max_new_q_value = model(new_state_tensor).max().item()
-
-            predicted_q_value = torch.tensor([iter_reward + discount_rate * max_new_q_value], device=old_q_value.device)
-            loss = loss_fn(old_q_value, predicted_q_value)
-            loss.backward()
-            optimizer.step()
-
-            if episode%display_rate == 0:
-                display.fill(game.black)
-                for body in snake_body:
-                    pygame.draw.rect(display, game.blue, [game.pixel_size * body.x, game.pixel_size*body.y, game.pixel_size, game.pixel_size])
-                pygame.draw.rect(display, game.red, [game.pixel_size*food.x, game.pixel_size*food.y, game.pixel_size, game.pixel_size])
-                pygame.display.update()
-                pygame.event.pump()
-                clock.tick(30)
-
-            if iter_reward == game.death_penalty:
-                break
-        if episode%display_rate == 0:
-            pygame.event.wait()
-            pygame.quit()
-        epsilon = max(np.exp(-epsilon_decay_rate * episode), epsilon_min)
-        
-        reward_list.append(episode_reward_count)
-except KeyboardInterrupt:
-    print("Keyboard interrupt saving model param")
-finally:
-    print(reward_list)
-    torch.save(model.state_dict(), model_path)
-
-
-
-sub_reward_list = reward_list[-500:]
-fig, ax = plt.subplots()
-ax.plot(sub_reward_list)
-plt.tight_layout()
-plt.show()
-
 
 
 
