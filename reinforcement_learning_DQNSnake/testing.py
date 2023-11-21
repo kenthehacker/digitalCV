9df738b8425395fe314c65e464f11bffad06bec6
kenthehacker <59815209+kenthehacker@users.noreply.github.com>
2023-11-20 19:36:05 -0800

    Delete reinforcement_learning_DQNSnake/testing.py

--- reinforcement_learning_DQNSnake/testing.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-SIZE = 10
-q_table = {}
-for i in range(-SIZE+1, SIZE):
-    for ii in range(-SIZE+1, SIZE):
-        for iii in range(-SIZE+1, SIZE):
-                for iiii in range(-SIZE+1, SIZE):
-                    q_table[((i, ii), (iii, iiii))] = [np.random.uniform(-5, 0) for i in range(4)]
-print(q_table[((9, 8), (1, 8))])
